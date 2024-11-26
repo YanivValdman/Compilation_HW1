75bd811e84180e94f847609598c65c0abd180037
Yaniv Valdman <yanivvaldman@campus.technion.ac.il>
2024-11-26 18:23:32 +0200

    new push

--- main.cpp
+++ main.cpp
@@ -2,13 +2,28 @@
 #include "output.hpp"
 #include <iostream>
 #include <string>
+#include <bits/stdc++.h>
+#include <assert.h>
+#define MIN_STR_LEN 2
 
-void handleString(char *yytext){
+const char *  handleString(char *yytext){
     // 1. Remove ""
     // 2. transform hex pattern to matching ascii character (e.g the substring "\x23" should be printed as #)
 
-    std::string s = input
+    std::string s = yytext;
+    std::cout << s << std::endl;
+    std::cout << "string len" << s.length() << std::endl;
+    assert(s.length() >= MIN_STR_LEN);
+    if(s.length() == MIN_STR_LEN) return "";
+    for (size_t i = 1; i < s.length() - 1; ++i) { //This loop ignores the first and last characters, ' " '
+        //start of an escape seq.
+        if (s[i] == '\\') {
+            //hex seq.
+            if(i + 1 <= s.length() && s[i + 1] == 'x');
+        }
+    }
 
+    return "a";
 }
 
 int main() {
@@ -17,7 +32,8 @@ int main() {
     while ((token = static_cast<tokentype>(yylex()))) {
         // your code here
         if(token == STRING) {
-            handleString(yytext);
+            const char * text = handleString(yytext);
+            output::printToken(yylineno, token, text);
         }
         else output::printToken(yylineno, token, yytext);
     }
